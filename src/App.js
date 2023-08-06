209ad75a2afcabce1731b15214d4d152f2b94417
Adewale Adetoro <adetorodev19@gmail.com>
2023-08-07 00:50:22 +0100

    Refractored

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ const App = () => {
     setSearchTerm(e.target.value)
   }
 
-  const searchedStories = stories.filter( story => 
+  const searchedStories = stories.filter(story =>
     story.title.toLowerCase().includes(searchTerm.toLowerCase())
   )
 
@@ -41,45 +41,30 @@ const App = () => {
       <hr />
 
       <List list={searchedStories} />
+
     </div>
   );
 };
 
-const List = (props) => {
-  props.list.map(item => {
-    return (
-      <div key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.point}</span>
-      </div>
-    );
-  });
-};
-
-
-
-const Search = props => {
-
-  // const [searchTerm, setSearchTerm] = useState("");
-
-  // const handleSearch = event => {
-  //   setSearchTerm(event.target.value);
-  // };
-
-  return (
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={props.handleSearch} />
-
-      <p>
-        {/* Searching for <strong>{searchTerm}</strong> */}
-      </p>
+const List = props =>
+  props.list.map(item => (
+    <div key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
     </div>
-  )
-}
+  ));
+
+
+
+const Search = props => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" onChange={props.onSearch} />
+  </div>
+);
 
 export default App;
