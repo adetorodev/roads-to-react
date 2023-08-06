0f9ed689e00cb68e4e9ea3aba4294970141ce2e3
Adewale Adetoro <adetorodev19@gmail.com>
2023-08-06 02:47:07 +0100

    working with list part a

--- src/App.js
+++ src/App.js
@@ -2,9 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 
-function getTitle(title){
-  return title
-}
+const list = [
+  {
+    title: 'React',
+    url: 'https://react.dev',
+    author: 'Ezekiel Adetoro',
+    num_comments: 3,
+    point: 4,
+    objectID: 0
+  },
+  {
+    title: 'Django',
+    url: 'https://djangoproject.orh',
+    author: 'Ezekiel Adetoro',
+    num_comments: 2,
+    point: 5,
+    objectID: 1
+  }
+]
 
 function App() {
   // const welcome = {
@@ -14,14 +29,32 @@ function App() {
   return (
     <div>
       <h1>
-        Hello{getTitle('React')}
+        My hacker Stories
       </h1>
 
       <label htmlFor='search'>Search: </label>
       <input id='search' type='text' />
+
+      <hr />
+
+      <List />
     </div>
   )
   }
 
+  function List() {
+    return list.map(function(item){
+      return(
+        <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.point}</span>
+          </div>
+      )
+    })
+  }
 
 export default App;
